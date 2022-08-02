e16f33e8c27cbe2eb5ebdc90b99c68b7177bf4b3
Lucas Portal <lucas.pborges@yahoo.com.br>
2022-08-02 14:52:46 -0300

    finalizando basics compose.

---
+++ composePathBasics/app/src/main/java/br/com/portal/composebasics/MainActivity.kt
@@ -0,0 +1,187 @@
+package br.com.portal.composebasics
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.portal.composebasics.ui.theme.ComposeBasicsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeBasicsTheme {
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun OnBoardScreen( onContinueClicked: () -> Unit ) {
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to the basics codelab")
+            Button(
+                modifier = Modifier.padding( vertical = 24.dp ),
+                onClick = onContinueClicked
+            ) {
+                Text(text = "Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320 )
+@Composable
+fun OnBoardScreenPreview() {
+    ComposeBasicsTheme {
+        OnBoardScreen( onContinueClicked = {} )
+    }
+}
+
+@Composable
+fun MyApp() {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf( true )}
+    if ( shouldShowOnboarding ) {
+        OnBoardScreen( onContinueClicked = { shouldShowOnboarding = false } )
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun Greetings(names: List<String> = List(1000) {"$it"} ) {
+    Surface( color = MaterialTheme.colors.background ) {
+        LazyColumn( modifier = Modifier.padding( vertical = 4.dp)) {
+            items(items = names) { name ->
+                Greeting(name = name)
+            }
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
+//fun Greeting(name: String) {
+//    val expanded = remember { mutableStateOf( false ) }
+//    val extraPadding by animateDpAsState(
+//        if ( expanded.value ) 48.dp else 0.dp,
+//        animationSpec = spring(
+//            dampingRatio = Spring.DampingRatioMediumBouncy,
+//            stiffness = Spring.StiffnessLow
+//        )
+//    )
+//    Surface(
+//        color = MaterialTheme.colors.primary,
+//        modifier = Modifier.padding( vertical = 4.dp, horizontal = 8.dp )
+//        ) {
+//        Row( modifier = Modifier.padding( 24.dp )) {
+//            Column(
+//                modifier = Modifier
+//                    .weight(1f)
+//                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+//            ) {
+//                Text(text = "Hello, ")
+//                Text(text = "$name!", style = MaterialTheme.typography.h4.copy(
+//                    fontWeight = FontWeight.ExtraBold
+//                ) )
+//            }
+//            Button(onClick = { expanded.value = !expanded.value }) {
+//                Text(text = if ( expanded.value ) "Show Less" else "Show More" )
+//            }
+//        }
+//    }
+//}
+
+@Composable
+private fun CardContent(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
+            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    ComposeBasicsTheme {
+        Greetings()
+    }
+}
